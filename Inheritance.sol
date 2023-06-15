7cb506352b8fbcde7732fe3d515f322fb2250254
Shweta Mandal <65994349+Shweta2003@users.noreply.github.com>
2023-06-15 20:15:08 +0530

    Create Inheritance.sol

---
+++ Inheritance.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// single inderitance
+// contract XYZ {
+//     function A1() public pure returns (string memory) {
+//         return "A1";
+//     }
+
+//     function A2() public pure returns (string memory) {
+//         return "A2";
+//     }
+// }
+
+// contract XYZ2 is XYZ {
+//     function A3() public pure returns (string memory) {
+//         return "A3";
+//     }
+// }
+
+
+
+// multilevel inhertance
+// contract XYZ {
+//     function A1() public pure returns (string memory) {
+//         return "A1";
+//     }
+
+//     function A2() public pure returns (string memory) {
+//         return "A2";
+//     }
+// }
+
+// contract XYZ2 is XYZ {
+//     function A3() public pure returns (string memory) {
+//         return "A3";
+//     }
+// }
+
+// contract XYZ3 is XYZ2{
+//     function A4() public pure returns (string memory) {
+//         return "A4";
+//     }
+// }
+
+
+
+// multiple inheritance
+// contract XYZ {
+//     function A1() public pure returns (string memory) {
+//         return "A1";
+//     }
+
+//     function A2() public pure returns (string memory) {
+//         return "A2";
+//     }
+// }
+
+// contract XYZ2 {
+//     function A3() public pure returns (string memory) {
+//         return "A3";
+//     }
+// }
+
+// contract XYZ3 is XYZ2,XYZ{
+//     function A4() public pure returns (string memory) {
+//         return "A4";
+//     }
+// }
